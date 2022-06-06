d9f176eb197b59698d38c22dfb4f762bbae0f012
Stardust <suminy@gmail.com>
2022-06-06 13:42:29 +0900

    해설 창 예쁘게 하기

--- room1.js
+++ room1.js
@@ -1,42 +1,42 @@
 const show1 = () => {
-    document.getElementById("show1").style.display = 'block'
-    document.getElementById("show2").style.display = "none";
-    document.getElementById("show3").style.display = "none";
-    document.getElementById("show4").style.display = "none";
+    document.getElementById("show1").style.right = "0%";
+    document.getElementById("show2").style.right = "-100%";
+    document.getElementById("show3").style.right = "-100%";
+    document.getElementById("show4").style.right = "-100%";
 }
 const show2 = () => {
-    document.getElementById("show2").style.display = 'block'
-    document.getElementById("show1").style.display = "none";
-    document.getElementById("show3").style.display = "none";
-    document.getElementById("show4").style.display = "none";
+    document.getElementById("show2").style.right = "0%";
+    document.getElementById("show1").style.right = "-100%";
+    document.getElementById("show3").style.right = "-100%";
+    document.getElementById("show4").style.right = "-100%";
 }
 const show3 = () => {
-    document.getElementById("show3").style.display = 'block'
-    document.getElementById("show1").style.display = "none";
-    document.getElementById("show2").style.display = "none";
-    document.getElementById("show4").style.display = "none";
+    document.getElementById("show3").style.right = "0%";
+    document.getElementById("show1").style.right = "-100%";
+    document.getElementById("show2").style.right = "-100%";
+    document.getElementById("show4").style.right = "-100%";
 }
 const show4 = () => {
-    document.getElementById("show4").style.display = 'block'
-    document.getElementById("show1").style.display = "none";
-    document.getElementById("show2").style.display = "none";
-    document.getElementById("show3").style.display = "none";
+    document.getElementById("show4").style.right = "0%";
+    document.getElementById("show1").style.right = "-100%";
+    document.getElementById("show2").style.right = "-100%";
+    document.getElementById("show3").style.right = "-100%";
 }
 document.getElementById("1").onclick = show1;
 document.getElementById("2").onclick = show2;
 document.getElementById("3").onclick = show3;
 document.getElementById("4").onclick = show4;
 const close1 = () => {
-    document.getElementById("show1").style.display = "none";
+    document.getElementById("show1").style.right = "-100%";
 }
 const close2 = () => {
-    document.getElementById("show2").style.display = "none";
+    document.getElementById("show2").style.right = "-100%";
 }
 const close3 = () => {
-    document.getElementById("show3").style.display = "none";
+    document.getElementById("show3").style.right = "-100%";
 }
 const close4 = () => {
-    document.getElementById("show4").style.display = "none";
+    document.getElementById("show4").style.right = "-100%";
 }
 document.getElementById("x1").onclick = close1;
 document.getElementById("x2").onclick = close2;
--- room3.js
+++ room3.js
@@ -1,22 +1,22 @@
 const show1 = () => {
-    document.getElementById("show1").style.display = 'block'
-    document.getElementById("show2").style.display = "none";
-    document.getElementById("show3").style.display = "none";
-    document.getElementById("show4").style.display = "none";
+    document.getElementById("show1").style.right = "0%";
+    document.getElementById("show2").style.right = "-100%";
+    document.getElementById("show3").style.right = "-100%";
+    document.getElementById("show4").style.right = "-100%";
 }
 const show2 = () => {
-    document.getElementById("show2").style.display = 'block'
-    document.getElementById("show1").style.display = "none";
-    document.getElementById("show3").style.display = "none";
-    document.getElementById("show4").style.display = "none";
+    document.getElementById("show2").style.right = "0%";
+    document.getElementById("show1").style.right = "-100%";
+    document.getElementById("show3").style.right = "-100%";
+    document.getElementById("show4").style.right = "-100%";
 }
 document.getElementById("1").onclick = show1;
 document.getElementById("2").onclick = show2;
 const close1 = () => {
-    document.getElementById("show1").style.display = "none";
+    document.getElementById("show1").style.right = "-100%";
 }
 const close2 = () => {
-    document.getElementById("show2").style.display = "none";
+    document.getElementById("show2").style.right = "-100%";
 }
 document.getElementById("x1").onclick = close1;
 document.getElementById("x2").onclick = close2;
